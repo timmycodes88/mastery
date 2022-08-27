321c1ee20322782ff7758dc8fe9a4819a8ed132e
Tim <tim@brightsidedeveloper.com>
2022-08-27 13:15:16 -0500

    Edits

--- src/Firebase/FirebaseConfig.js
+++ src/Firebase/FirebaseConfig.js
@@ -1,5 +1,4 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
+
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -10,6 +9,3 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID, 
   measurementId: process.env.REACT_APP_MEASUREMENT_ID, 
 }
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
